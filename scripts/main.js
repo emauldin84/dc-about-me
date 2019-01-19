6be70bb8731410249d07d7499986965412e51b33
Eric Mauldin <emauldin84@gmail.com>
2019-01-19 10:42:38 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/Eric.png') {
+    myImage.setAttribute ('src', 'images/Eric-freckles.png');
+  } else if (mySrc === 'images/Eric-freckles.png') {
+    myImage.setAttribute ('src', 'images/Eric-freckles-horns.png');
+  } else if (mySrc === 'images/Eric-freckles-horns.png') {
+    myImage.setAttribute ('src', 'images/Eric-freckles-horns-patch.png');
+  } else if (mySrc === 'images/Eric-freckles-horns-patch.png') {
+    myImage.setAttribute ('src', 'images/Eric-freckles-horns-patch-earring.png');
+  } else {
+    myImage.setAttribute ('src', 'images/Eric.png')
+  }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to Eric\'s About Me page, ' + myName + '!';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to Eric\'s About Me page, ' + storedName +'!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
